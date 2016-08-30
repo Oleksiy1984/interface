b4e9c9a6b984839d68e7023fef729d5df4adf3ce
Oleksiy1984 <o_orlovskiy@ukr.net>
2016-08-30 23:54:30 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String args[]) {
+        Istack mystack;
+        Istack.istatic();
+
+        // create an interface reference variable
+        DynStack ds = new DynStack(5);
+        FixedStack fs = new FixedStack(8);
+        mystack = ds;
+
+        // load dynamic stack
+        // push some numbers onto the stack
+        for(int i=0; i<12; i++)
+            mystack.push(i);
+        mystack = fs;
+
+        // load fixed stack
+        for(int i=0; i<8; i++)
+         mystack.push(i);
+
+        mystack = ds;
+        System.out.println("Values in dynamic stack:");
+        for(int i=0; i<12; i++)
+            System.out.println(mystack.pop());
+        ds.clear();
+        mystack = fs;
+        System.out.println("Values in fixed stack:");
+        for(int i=0; i<8; i++)
+            System.out.println(mystack.pop());
+        fs.clear();
+
+    }
+}
